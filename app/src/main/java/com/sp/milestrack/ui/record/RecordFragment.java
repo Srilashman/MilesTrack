752f6e37dff484e8ac67a3f71a57687db1b6ad01
Srilashman <srilashman.sg@gmail.com>
2025-01-27 17:48:25 +0800

    Add spinner (dropdown menu) in record tab

--- app/src/main/java/com/sp/milestrack/ui/record/RecordFragment.java
+++ app/src/main/java/com/sp/milestrack/ui/record/RecordFragment.java
@@ -1,9 +1,12 @@
 package com.sp.milestrack.ui.record;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.sp.milestrack.R;
 import com.sp.milestrack.databinding.FragmentRecordBinding;
 
 public class RecordFragment extends Fragment {
@@ -25,8 +29,19 @@ public class RecordFragment extends Fragment {
         binding = FragmentRecordBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        recordViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textGallery;
+//        recordViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Spinner spinner = (Spinner) root.findViewById(R.id.sport_choice);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.sports_array,
+                R.layout.spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //spinner.setBackgroundColor(Color.rgb(0, 255, 0));
+        // Apply the adapter to the spinner.
+        spinner.setAdapter(adapter);
         return root;
     }
 
