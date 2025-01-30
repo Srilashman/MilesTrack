b1fc77f2d6f0a87eef1d83cd8d369ca1635b7b80
Clarence <CLARENCEENG.23@ichat.sp.edu.sg>
2025-01-30 18:04:40 +0800

    Add row.xml and indoor form ui

--- app/src/main/java/com/sp/milestrack/ui/list/ListFragment.java
+++ app/src/main/java/com/sp/milestrack/ui/list/ListFragment.java
@@ -24,8 +24,8 @@ public class ListFragment extends Fragment {
         binding = FragmentListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        listViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textSlideshow;
+//        listViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
