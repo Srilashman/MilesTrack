0daf140c584d966e3ffb9d9a66ee55dfdc81245f
Srilashman <srilashman.sg@gmail.com>
2025-02-13 04:15:45 +0800

    Add workout plan and edting

--- app/src/main/java/com/sp/milestrack/Database.java
+++ app/src/main/java/com/sp/milestrack/Database.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class Database extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "userinfo.db";
@@ -18,6 +19,8 @@ public class Database extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         // Will be called once when the database is not created
         db.execSQL("CREATE TABLE userinfo_table (_id INTEGER PRIMARY KEY AUTOINCREMENT," + " date TEXT, height REAL, weight REAL, age REAL, weightlossgoal TEXT);");
+        db.execSQL("CREATE TABLE trainingschedule_table (_id INTEGER PRIMARY KEY AUTOINCREMENT," + " start_date TEXT, end_date TEXT);");
+        db.execSQL("CREATE TABLE trainingplans_table (_id INTEGER PRIMARY KEY AUTOINCREMENT," + " exercise TEXT, distance DECIMAL(10, 2), intensity TEXT, status BIT, date TEXT);");
     }
 
     @Override
@@ -30,6 +33,153 @@ public class Database extends SQLiteOpenHelper {
     public Cursor getAll() {
         return (getReadableDatabase().rawQuery("SELECT _id, date, height, weight, age, weightlossgoal FROM userinfo_table", null));
     }
+
+    public long insertTrainingDate(String start_date, String end_date) {
+        ContentValues cv = new ContentValues();
+        cv.put("start_date", start_date);  // Add date to ContentValues
+        cv.put("end_date", end_date);
+        long userId = getWritableDatabase().insert("trainingschedule_table", "null", cv);
+
+        getWritableDatabase().close();
+        return userId;
+    }
+    
+    public long insertTrainingPlans(String exercise, double distance, String intensity,String date, boolean status) {
+        ContentValues cv = new ContentValues();
+        cv.put("exercise", exercise);
+        cv.put("distance", distance);
+        cv.put("intensity", intensity);
+        cv.put("status", status);
+        cv.put("date", date);
+        long userId = getWritableDatabase().insert("trainingplans_table", null, cv);
+
+        getWritableDatabase().close();
+        return userId;
+    }
+
+    public void updateTrainingPlansByDate(String date, boolean status, double dist, String exercise) {
+        ContentValues cv = new ContentValues();
+        String[] args = {date};
+        cv.put("exercise", exercise);
+        cv.put("distance", dist);
+        cv.put("status", status);
+        getWritableDatabase().update("trainingplans_table", cv, "date = ?", args);
+    }
+
+    public void updateTrainingPlans(String id, String exercise, double distance, String intensity,String date, boolean status) {
+        ContentValues cv = new ContentValues();
+        String[] args = {id};
+        cv.put("exercise", exercise);
+        cv.put("distance", distance);
+        cv.put("intensity", intensity);
+        cv.put("status", status);
+        getWritableDatabase().update("trainingplans_table", cv, "_ID = ?", args);
+    }
+
+    public void deleteTraining() {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            db.execSQL("DELETE FROM trainingplans_table");
+            db.execSQL("DELETE FROM trainingschedule_table");
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+            db.close();
+        }
+    }
+
+
+    public void updateTrainingDate(String start_date, String end_date) {
+        ContentValues cv = new ContentValues();
+        String[] args = {"1"};
+        cv.put("start_date", start_date);
+        cv.put("end_date", end_date);
+        getWritableDatabase().update("trainingschedule_table", cv, "_ID = ?", args);
+    }
+
+    public boolean isPlansSet() {
+        Cursor cursor = getReadableDatabase().rawQuery("SELECT _id, start_date, end_date FROM trainingschedule_table", null);
+        boolean hasData = (cursor != null && cursor.moveToFirst());
+        if (cursor != null) cursor.close();
+        return hasData;
+    }
+
+
+    public Cursor getAllTrainingPlans() {
+        return (getReadableDatabase().rawQuery("SELECT _id, exercise, distance, intensity, status, date FROM trainingplans_table", null));
+    }
+
+    public Cursor getAllTrainingDates() {
+        return (getReadableDatabase().rawQuery("SELECT _id, start_date, end_date FROM trainingschedule_table", null));
+    }
+
+    public String getSportFromDate(String date) {
+        String[] args = {date};
+        String ex = null;
+        Cursor cursor = null; // Declare cursor
+
+        try {
+            cursor = getReadableDatabase().rawQuery(
+                    "SELECT exercise FROM trainingplans_table WHERE date = ?", args); // Use correct column name
+
+            if (cursor != null && cursor.moveToFirst()) {
+                ex = cursor.getString(0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Print error to log
+        } finally {
+            if (cursor != null) {
+                cursor.close(); // Always close cursor
+            }
+        }
+        return ex;
+    }
+
+    public double getDistFromDate(String date) {
+        String[] args = {date};
+        double ex = -1;
+        Cursor cursor = null; // Declare cursor
+
+        try {
+            cursor = getReadableDatabase().rawQuery(
+                    "SELECT distance FROM trainingplans_table WHERE date = ?", args); // Use correct column name
+
+            if (cursor != null && cursor.moveToFirst()) {
+                ex = cursor.getDouble(0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Print error to log
+        } finally {
+            if (cursor != null) {
+                cursor.close(); // Always close cursor
+            }
+        }
+        return ex;
+    }
+
+    public int getStatusFromDate(String date) {
+        String[] args = {date};
+        int ex = -1;
+        Cursor cursor = null; // Declare cursor
+
+        try {
+            cursor = getReadableDatabase().rawQuery(
+                    "SELECT status FROM trainingplans_table WHERE date = ?", args); // Use correct column name
+
+            if (cursor != null && cursor.moveToFirst()) {
+                ex = cursor.getInt(0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Print error to log
+        } finally {
+            if (cursor != null) {
+                cursor.close(); // Always close cursor
+            }
+        }
+        return ex;
+    }
+
 
     public boolean ifPromptsDone() {
         Cursor cursor = getReadableDatabase().rawQuery("SELECT _id, height, weight, age, weightlossgoal FROM userinfo_table", null);
---
+++ app/src/main/java/com/sp/milestrack/ui/edit_info/editWorkout.java
@@ -0,0 +1,108 @@
+package com.sp.milestrack.ui.edit_info;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sp.milestrack.R;
+import com.sp.milestrack.databinding.FragmentEditInfoBinding;
+import com.sp.milestrack.databinding.FragmentEditWorkoutBinding;
+import com.sp.milestrack.Database;
+public class editWorkout extends Fragment {
+    private FragmentEditWorkoutBinding binding;
+    TextView dateView;
+    EditText editDist;
+    Button finishEdit;
+    String sport;
+    String date;
+    double dist;
+    int status;
+    CheckBox checkAsComplete;
+    private Database helper = null;
+
+
+    public editWorkout() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentEditWorkoutBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        Spinner spinner = (Spinner) root.findViewById(R.id.sport_choice_editing);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                getActivity(),
+                R.array.sports_array,
+                R.layout.spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Specify the layout to use when the list of choices appears.
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Bundle bundle = getArguments();
+        sport = "";
+        date = "";
+        dist = -1;
+        if (bundle != null) {
+            sport = bundle.getString("sport", "");
+            date = bundle.getString("date", "");
+            dist = bundle.getDouble("dist", -2);
+            status = bundle.getInt("status", status);
+        }
+//        Toast.makeText(getContext(), sport, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getContext(), date, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getContext(), String.valueOf(dist), Toast.LENGTH_SHORT).show();
+        spinner.setAdapter(adapter);
+        if (sport != null) {
+            int spinnerPosition = adapter.getPosition(sport);
+            spinner.setSelection(spinnerPosition);
+        }
+        dateView = root.findViewById(R.id.workout_date);
+        dateView.setText(date);
+        editDist = root.findViewById(R.id.editDistanceTarget);
+        editDist.setHint(String.valueOf(dist));
+        finishEdit = root.findViewById(R.id.finish_edit);
+        finishEdit.setOnClickListener(checkInputs);
+        checkAsComplete = root.findViewById(R.id.checkbox_completed);
+        helper = new Database(requireContext());
+        return root;
+    }
+    private View.OnClickListener checkInputs = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Spinner spinner = requireView().findViewById(R.id.sport_choice_editing);
+            String text = (spinner.getSelectedItem() != null) ? spinner.getSelectedItem().toString() : sport;
+            if (text == sport && editDist.getText() == null && checkAsComplete.isChecked() == (status != 0)) {
+                return; // no change Double.parseDouble(String.valueOf(editDist.getText()))
+            } else if (editDist.getText().toString().isEmpty()) {
+                helper.updateTrainingPlansByDate(date, (checkAsComplete.isChecked()), dist, text);
+                NavController navController = Navigation.findNavController(requireView());
+                navController.navigate(R.id.action_finish_edit);
+            } else {
+                helper.updateTrainingPlansByDate(date, (checkAsComplete.isChecked()), Double.parseDouble(editDist.getText().toString()), text);
+                NavController navController = Navigation.findNavController(requireView());
+                navController.navigate(R.id.action_finish_edit);
+            }
+        }
+    };
+}
